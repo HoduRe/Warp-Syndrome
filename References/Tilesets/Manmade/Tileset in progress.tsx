6423e2ca98fdd908b5a34ddad6135ce9f99aa92f
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2019-10-09 19:05:44 +0200

    Finished basic TileSet
    Updates:
    -Created all the tiles needed to build the first basic level of the game.
    -Moved that into a new folder

--- References/Tilesets/Manmade/Tileset in progress.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tileset in progress" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="432" columns="24">
- <image source="Tileset in progress.png" trans="ffffff" width="800" height="600"/>
-</tileset>
